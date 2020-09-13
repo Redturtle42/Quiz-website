77e0e96358f83539561ea1bd2059437414beef13
Redturtle42 <izabella.tanacs@yahoo.com>
2020-09-13 17:36:55 +0200

    working on result page

--- public/js/questions.js
+++
@@ -1,9 +0,0 @@
-let previous_question = () => {
-    alert("Prev");
-};
-let next_question = () => {
-    alert("Next");
-};
-let pressed = () => {
-    alert("You pressed");
-};
--- routes/result.js
+++ routes/result.js
@@ -4,9 +4,45 @@ const knex = require('../db/knex');
 const router = express.Router();
 
 /* GET question page. */
-router.get('/', async function (req, res, next) {
-    const result = await knex('user_answers').select();
-    res.render('result-score', { title: 'Result', result });
+router.post('/', async function (req, res, next) {
+    const quiz_result = Object.entries(req.body);       //Parse object to array
+
+    let count = 0;
+    for (let i = 0; i < quiz_result.length; i++) {
+        const question_id = quiz_result[i][0];
+        const user_answer = quiz_result[i][1];
+
+        let correct_answer = await knex('answers')
+            .select('is_correct', 'answers.question').from('answers')
+            .where('answers.id', '=', question_id)
+        correct_answer = correct_answer[0].is_correct;      //unpack RowDataPacket
+
+        if (user_answer == correct_answer) {
+            count++;
+        }
+    };
+
+    let categoryId = await knex('answers')
+        .select('category_id').from('answers')
+        .where('id', '=', quiz_result[0][0])
+
+    categoryId = categoryId[0].category_id;
+
+    console.log(categoryId);
+
+    let question_amount = await knex('answers')
+        .select('category_id').count('*', { as: 'sumOfQuestion' })
+        .from('answers')
+        .where('category_id', '=', categoryId)
+        .groupBy('category_id');
+
+    question_amount = question_amount[0].sumOfQuestion;
+
+    console.log(question_amount);
+
+
+    res.render('result-score', { title: 'Result', correct: count, total: question_amount });
 });
+
 
 module.exports = router;
