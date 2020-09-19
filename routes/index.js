6623b23ad64349073e622adc633b018a596920c4
Redturtle42 <izabella.tanacs@yahoo.com>
2020-09-19 18:22:15 +0200

    ready v1.0

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const knex = require('../db/knex');
 var router = express.Router();
 
 /* GET home page on localhost:3004 */
-
 router.get('/', async function (req, res, next) {
   const categories = await knex('categories').select();
   res.render('index', { title: "Welcome to IT Quiz", categories });
--- routes/stat.js
+++ routes/stat.js
@@ -2,19 +2,20 @@ const express = require('express');
 const knex = require('../db/knex');
 const router = express.Router();
 
-router.get('/:id', async function (req, res, next) {
-    const type_id = req.params.id;
+router.get('/:type', async function (req, res, next) {
+    const typeOfQuiz = req.params.type;
 
     let stat = await knex('stat')
         .select().from("stat")
         .avg("percentage", { as: "percentage" })
         .join('categories', 'categories.id', '=', 'type_id')
-        .where('categories.type', "=", type_id)
+        .where('categories.type', "=", typeOfQuiz)
         .groupBy("type_id")
 
+    // cut of decimal values an unpack RowDataPacket
     stat = Math.round(stat[0].percentage);
 
-    res.render('statistics-page', { title: "Statistics", stat, type_id });
+    res.render('statistics-page', { title: "Statistics", stat, type_id: typeOfQuiz });
 });
 
 module.exports = router;
